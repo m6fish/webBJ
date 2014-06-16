c89a3518bfca24ddea79b8e3f82f79d14a45846c
Rfish <haopingyu@kkbox.com>
2014-06-16 15:55:58 +0800

    phpcs

--- php/receiver.php
+++ php/receiver.php
@@ -5,53 +5,52 @@ include("game.php");
 
 try {
 
-if (!isset($_SESSION["obj"])) {
-    $bj = new Game(2);
-    $bj->prepare();
-    $first_p1 = $bj->getFirstCard("p1");
-    $first_b = $bj->getFirstCard("b");
-    $_SESSION["obj"] = serialize($bj);
-}
-
-if (isset($_POST["moreCard"])) {
-    $obj = unserialize($_SESSION["obj"]);
-    $mode = $_POST["moreCard"];
-    if (0 == $mode) {
-        $newCardtxt = $obj->addPlayerCard("p1");
-        $check = $obj->check("p1");
-        $resultArr = array('p1' => $newCardtxt,
-                'score_p1' => $check
-            );
-
-        echo json_encode($resultArr);
-    
-    } elseif (1 == $mode) {
-        $sum_p1 = $obj->check("p1");
-        $sum_b = $obj->check("b");
-        while ($sum_b < 17) {
-            $newCard_b[] = $obj->addPlayerCard("b");
-
-            $sum_b = $obj->check("b");
-        }
-
-        $win = $obj->setResult();
-        echo json_encode(
-            array('score_p1'=>$sum_p1,
-                'score_b'=>$sum_b,
-                'b_rest'=>$newCard_b,
-                'result'=> $win
-            )
-        );
-    } elseif (2 == $mode) {
-        echo json_encode(array("b" => $first_b, "p1" => $first_p1));
+    if (!isset( $_SESSION["obj"])) {
+        $bj = new Game(2);
+        $bj->prepare();
+        $first_p1 = $bj->getFirstCard("p1");
+        $first_b = $bj->getFirstCard("b");
+        $_SESSION["obj"] = serialize($bj);
     }
 
+    if (isset($_POST["moreCard"])) {
+        $obj = unserialize($_SESSION["obj"]);
+        $mode = $_POST["moreCard"];
+        if (0 == $mode) {
+            $newCardtxt = $obj->addPlayerCard("p1");
+            $check = $obj->check("p1");
+            $resultArr = array('p1' => $newCardtxt,
+                    'score_p1' => $check
+                );
 
-    $_SESSION["obj"] = serialize($obj);
-}
+            echo json_encode($resultArr);
+        
+        } elseif (1 == $mode) {
+            $sum_p1 = $obj->check("p1");
+            $sum_b = $obj->check("b");
+            while ($sum_b < 17) {
+                $newCard_b[] = $obj->addPlayerCard("b");
 
-}
-catch (Exception $e) {
+                $sum_b = $obj->check("b");
+            }
+
+            $win = $obj->setResult();
+            echo json_encode(
+                array('score_p1'=>$sum_p1,
+                    'score_b'=>$sum_b,
+                    'b_rest'=>$newCard_b,
+                    'result'=> $win
+                )
+            );
+        } elseif (2 == $mode) {
+            echo json_encode(array("b" => $first_b, "p1" => $first_p1));
+        }
+
+
+        $_SESSION["obj"] = serialize($obj);
+    }
+
+} catch (Exception $e) {
     $message = "Error! " . $e->getMessage() . "\n";
     echo "<script> alert('$message')</script>";
 }
